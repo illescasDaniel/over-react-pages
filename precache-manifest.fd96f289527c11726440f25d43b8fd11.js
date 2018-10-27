755134cb5d26920dcfa033237c558c196d7022ca
Daniel Illescas <illescas.daniel@protonmail.com>
2018-10-27 21:10:10 +0200

    Updates

---
+++ precache-manifest.fd96f289527c11726440f25d43b8fd11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/over-react-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ce405c8b31f6202d78e1",
+    "url": "/over-react-pages/static/js/runtime~main.ce405c8b.js"
+  },
+  {
+    "revision": "035695ea7afb34b7f844",
+    "url": "/over-react-pages/static/js/main.035695ea.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/over-react-pages/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "035695ea7afb34b7f844",
+    "url": "/over-react-pages/static/css/main.05ce2748.chunk.css"
+  },
+  {
+    "revision": "6aae9e749b7306326ef76ba300d39495",
+    "url": "/over-react-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/over-react-pages/precache-manifest.1156dda9fc4432b916735bb2b6cf3ab2.js"
+  "/over-react-pages/precache-manifest.fd96f289527c11726440f25d43b8fd11.js"
 );
 
 workbox.clientsClaim();
